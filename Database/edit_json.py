5b6c4f267b5beeb28b7d823586563f186424835e
Mike Newby <michaelnewby07@gmail.com>
2023-10-18 16:23:07 -0400

    Figured out formatting for new JSON File
    Still need to figure out how to get the program to filter out the elements with 'step' attributes.  It's close to working properly, but not quite there yet.

--- Database/edit_json.py
+++ Database/edit_json.py
@@ -3,28 +3,147 @@ import json
 def read_json(JSON_File):
     count = 0
     for i in JSON_File['elements']:
-        try:
-            print("ID: ", i["id"], "\t|\t", "Tags: ", i["tags"])
-        except KeyError:
-            print("ID: ", i["id"], "\t|\t", "Tags: None")
-            # continue
+        # try:
+            # print("ID: ", i["id"], "\t|\t", "Tags: ", i["tags"])
+        # except KeyError:
+            # print("ID: ", i["id"], "\t|\t", "Tags: None")
         count += 1
     f.close()
     return count
 
 
-def new_JSON(JSON_file):
-    # f = open(JSON_file)
+# def remove_steps(JSON_file):
+#     f = open("new_file.json", "w")
+#     f.write("{\"elements\": [")
+#     count = 0
+#     outer_current = ""
+#     for a in JSON_file['elements']:
+#         count += 1
+#         current = str(a)
+#         for elem in current:
+#             if (elem == "\'"):
+#                 outer_current += "\""
+#             else:
+#                 outer_current += elem
+#         outer_current += ", "
+#     outer_current = outer_current.rstrip(outer_current[-1])
+#     outer_current = outer_current.rstrip(outer_current[-1])
+#
+#     outer_current += "]}"
+#
+#     print(outer_current)
+#
+#     f.writelines(outer_current)
+#     # for i in JSON_file['elements']:
+#     #     try:
+#     #         # print("ID: ", i["id"], "\t|\t", "Tags: ", i["tags"])
+#     #         print(i)
+#     #     except KeyError:
+#     #         print("ID: ", i["id"], "\t|\t", "Tags: None")
+#     # f.write("]")
+#     # outer_current += "}"
+#
+#     f.close()
+#     return count;
+
+def remove_steps(JSON_file):
     f = open("new_file.json", "w")
+    f.write("{\"elements\": [")
+    count = 0
+    outer_current = ""
+    for a in JSON_file['elements']:
+        # print(a)
+        try:
+            if (a["tags"]["highway"] == "steps"):
+                continue
+        except KeyError:
+            pass
+        else:
+            count += 1
+            current = str(a)
+            for elem in current:
+                if (elem == "\'"):
+                    outer_current += "\""
+                else:
+                    outer_current += elem
+            outer_current += ", "
+    outer_current = outer_current.rstrip(outer_current[-1])
+    outer_current = outer_current.rstrip(outer_current[-1])
 
-    for a in data['elements']:
+    outer_current += "]}"
 
+    # print(outer_current)
+
+    f.writelines(outer_current)
+    # for i in JSON_file['elements']:
+    #     try:
+    #         # print("ID: ", i["id"], "\t|\t", "Tags: ", i["tags"])
+    #         print(i)
+    #     except KeyError:
+    #         print("ID: ", i["id"], "\t|\t", "Tags: None")
+    # f.write("]")
+    # outer_current += "}"
+
+    f.close()
+    return count;
+
+# def remove_steps(JSON_file):
+#     f = open("new_file.json", "w")
+#     count = 0
+#     # for a in JSON_file['elements']:
+#     #     f.writelines(a)
+#     #     count = 0
+#     f.write("\"elements\": [")
+#
+#     # dict = [{1: "hello", 2:"greetings", 3:"howdy"}, {1: "bye", 2: "goodbye", 3:"adios"}]
+#     # f.write(str(dict))
+#     for i in JSON_file['elements']:
+#         try:
+#             if (i["tags"]["highway"] == "steps"):
+#                 continue
+#             # print("ID: ", i["id"], "\t|\t", "Tags: ", i["tags"])
+#             # print(i)
+#         except KeyError:
+#             # temp_dict = i
+#             # print(temp_dict)
+#             # f.write(str(temp_dict))
+#             # f.write("\n")         # f.writelines("{\"", i, "\"}: ", i[])
+#             continue
+#         # for j in i:
+#
+#             # f.writelines("\"" + str(j) + "\": "+ str(i[j]) + "}")
+#                 # f.writelines(i)
+#         count += 1
+#     f.write("]")
+#     # print("\nMOVING ON...")
+#     # for i in f['elements']:
+#     #     print(i);
+#     #     count +=1
+#     # f.close()
+#
+#     return count;
 
 
 
 filename = '4645508147a65be4e31c1cce9ade2840c6ac334f.json';
 f = open(filename)
 data = json.load(f)
-orig_count = read_json(filename)
+orig_count = read_json(data)
+new_count = remove_steps(data)
 
-print("There are ", orig_count, " elements in this json.")
+print("There are", orig_count, "elements in the original json.")
+print("There are", new_count, "elements in the new json.")
+# step_count = 0
+#
+# for i in data["elements"]:
+#     try:
+#         if (i["tags"]["highway"] == "steps"):
+#             print("steps")
+#             step_count += 1
+#     except KeyError:
+#         continue
+#
+# print(step_count)
+
+# for i in data['elements']:
+#     print(i)
