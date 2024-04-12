102f84b488f8debd9a3878238949a7e00110d8f0
dino.benj <dino.benj@gmail.com>
2024-04-12 16:56:08 -0400

    Cleaned Up code by removing unused variables and writing documentation.

--- Django/backend/routedb/coppyGenerate.py
+++ Django/backend/routedb/coppyGenerate.py
@@ -9,13 +9,28 @@ import pandas as pd
 from entrancePoint import entrancePoint
 
 
+"""
+testPlotRoute(MultiDiGraph RPI)
+This function is used to test the plotRoute function. 
+It is not used in the final implementation of the project.
+Parameters: RPI - the graph of RPI
+"""
 def testPlotRoute(RPI):
+      
       orig = ox.nearest_nodes(RPI, 42.7324294, -73.6905807)
       dest = ox.nearest_nodes(RPI, 42.7338301, -73.6847063)
       #1208042175, "lat": 42.7268945, "lon": -73.6737245
       route = nx.shortest_path(RPI, orig, dest, 'travel_time')
       return route
 
+"""
+closestNode(MultiDiGraph G, int x, int y, int orig_id)
+This function is used to find the closest node to a given point.
+Parameters: G - the graph of RPI, 
+            x - the x coordinate of the point, 
+            y - the y coordinate of the point, 
+            orig_id - the id of the original node
+"""
 def closestNode(G, x, y, orig_id):
     min_dist = float('inf')
     closest_node_id = None
@@ -31,6 +46,12 @@ def closestNode(G, x, y, orig_id):
             min_dist = tmp_dist
     return closest_node_id
 
+"""
+createNodeArray(string filename)
+This function is used to create a list of entrance points from a json file.
+Parameters: filename - the name of the json file
+This function calls the class entrancePoint from entrancePoint.py
+"""
 def createNodeArray(filename):
       f = open(filename)
       data = json.load(f)
@@ -51,7 +72,12 @@ def createNodeArray(filename):
       gdf = gpd.GeoDataFrame(nodeArr)
       return gdf
 
-
+"""
+plotGraph(GeoDataFrame new_nodes)
+This function is used to plot the graph of RPI with the new nodes added.
+Parameters: new_nodes - the new nodes to be added to the graph
+This function is used to generate a barebones graph of RPI with the new nodes added as entrances
+"""
 def plotGraph(new_nodes):
       ox.settings.log_console = False
       ox.settings.use_cache = True
@@ -67,20 +93,11 @@ def plotGraph(new_nodes):
       new_node_gdf = gpd.GeoDataFrame(new_nodes)
       new_node_gdf = new_node_gdf.set_crs(4326, allow_override=True)
       nodes = nodes.set_crs(4326, allow_override=True)  
-      non_new_nodes = nodes  
       nodes = pd.concat([nodes, new_node_gdf], ignore_index=False)
       entrance_df = nodes[nodes['highway'].str.contains("Entrance", na=False)]
-      counter = len(new_nodes)
-      new_edge_list = []
       RPI = ox.graph_from_gdfs(nodes, edges)
       edgesToRemove = []
-      for a, b,c,d in RPI.edges(keys = True, data = True):
-            # print("=====")
-            # print(a)
-            # print(b)
-            # print(c)
-            # print(d)
-            # print("=====")
+      for a, b,_,d in RPI.edges(keys = True, data = True):
             if 'highway' in d.keys():
                   if 'steps' in d['highway']:
                         edgesToRemove.append((a, b))
@@ -89,7 +106,7 @@ def plotGraph(new_nodes):
       for i in edgesToRemove:
             print(i[0], i[1])
             RPI.remove_edge(i[0], i[1])
-      for index, row in entrance_df.iterrows():
+      for _, row in entrance_df.iterrows():
             tmp_orig_id = int(row['id'])
             tmp_x = float(row['x'])  
             tmp_y = float(row['y'])          
@@ -98,7 +115,7 @@ def plotGraph(new_nodes):
                   RPI.add_edge(tmp_dest_id, tmp_orig_id)
                   RPI.add_edge(tmp_orig_id, tmp_dest_id)   
       RPI = ox.add_edge_speeds(RPI,5)   
-      for a, b,c,d in RPI.edges(keys = True, data = True):
+      for a, b,_,d in RPI.edges(keys = True, data = True):
             if 'length' not in d.keys():
                   d['length'] = 0
       G = RPI
@@ -109,15 +126,26 @@ def plotGraph(new_nodes):
             pickle.dump(G, f)
       route = ox.shortest_path(G, 6, 9)
       print(route)
-      fig, ax = ox.plot_graph_route(G, route, route_color='r', route_linewidth=6, node_size=5)
+      _, _ = ox.plot_graph_route(G, route, route_color='r', route_linewidth=6, node_size=5)
       return RPI
 
+"""
+readGraphFromFile() -> MultiDiGraph
+This function is used to read the graph from a file. This is a preset path as django needs a preset path
+returns a MultiDiGraph object
+"""
 def readGraphFromFile():
-      with open("/home/dennib2/Database/Django/backend/routedb/Accessible_Graph.p", 'rb') as f: 
+      with open("~/routedb/Accessible_Graph.p", 'rb') as f: 
             G_loaded = pickle.load(f)
             for i in G_loaded.edges(keys = True, data = True):
                   print(i)
             return G_loaded
+      
+"""
+bensRoute(string start, string end) -> list
+This function is used to find the shortest path between two entrances.
+This is more a test function and is not used in the final implementation of the project. route is printed to the console
+"""
 def bensRoute(start, end):
       try:
             place = 'Rensselaer Polytechnic Institute'
@@ -134,14 +162,18 @@ def bensRoute(start, end):
             for i in range(len(route_list)-1):
                   a = test.get_edge_data(route_list[i], route_list[i+1])[0]
                   print(route[i])
-                  #print(route_list[i], route_list[i+1], a)
-            #fig, ax = ox.plot_graph_route(test, route, route_color='r', route_linewidth=6, node_size=1)
             ox.plot_graph(test)
             return route_list
       except Exception as e:
             print('exception', e)
             return []
       
+
+"""
+routemaker(string start, string end) -> list
+This function is used to find the shortest path between two entrances.
+Parameters: start - the starting entrance, end - the ending entrance
+"""
 def routemaker(start, end):
       try:
             place = 'Rensselaer Polytechnic Institute'
@@ -150,8 +182,7 @@ def routemaker(start, end):
             entrance_df = nodes[nodes['highway'].str.contains("Entrance", na=False)]
             start_node = int(entrance_df.loc[entrance_df['highway'] == start]['id'].values[0].item())
             end_node = int(entrance_df.loc[entrance_df['highway'] == end]['id'].values[0].item())
-            route = nx.shortest_path(test, start_node, end_node, 'travel_time')
-      
+            route = nx.shortest_path(test, start_node, end_node, 'travel_time')     
             route_list = []
             for i in route:
                   node = {}
@@ -164,8 +195,10 @@ def routemaker(start, end):
             print('exception', e)
             return []
 
-readGraphFromFile()
-# new_nodes = createNodeArray('/home/dennib2/Database/Django/backend/routedb/buildingEntrance.json')
-# plotGraph(new_nodes)
-# testRoute = bensRoute("Entrance_West Hall", "Entrance_Academy Hall")
-# print(testRoute)
+
+if __name__ == "__main__":
+      readGraphFromFile()
+      new_nodes = createNodeArray('/home/dennib2/Database/Django/backend/routedb/buildingEntrance.json')
+      plotGraph(new_nodes)
+      testRoute = bensRoute("Entrance_West Hall", "Entrance_Academy Hall")
+      print(testRoute)
